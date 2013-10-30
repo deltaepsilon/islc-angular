02ca580e59cf33529d2271098e88d3bfaf4a95c6
Chris Esplin <christopher.esplin@gmail.com>
2013-10-29 19:10:21 -0600

    Cart styling.

--- app/scripts/controllers/Login.js
+++ app/scripts/controllers/Login.js
@@ -1,9 +1,12 @@
 'use strict';
 
 angular.module('islcAngularApp')
-  .controller('LoginCtrl', function ($scope, $state, $stateParams) {
-    $scope.redirect = '#' + ($scope.$previousState || '/');
-    $scope.origin = '#' + ($state.current.url || '/');
-    $scope.token = $stateParams.token;
+  .controller('LoginCtrl', function ($scope, $state, $stateParams, $timeout) {
+    $timeout(function () {
+      $scope.redirect = '#' + ($scope.$previousState ? $scope.$previousState.url : '/');
+      $scope.origin = '#' + ($state.current.url || '/');
+      $scope.token = $stateParams.token;
+    });
+
 
   });
