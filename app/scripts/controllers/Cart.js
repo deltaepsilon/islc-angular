69604865490a538f992614d5d4e24b7df475d56a
Chris Esplin <christopher.esplin@gmail.com>
2013-11-05 17:42:45 -0700

    Removing extra watch

--- app/scripts/controllers/Cart.js
+++ app/scripts/controllers/Cart.js
@@ -38,11 +38,6 @@ angular.module('islcAngularApp')
     $scope.months = ['Expiration Month', 'January (1)', 'February (2)', 'March (3)', 'April (4)', 'May (5)', 'June (6)', 'July (7)', 'August (8)', 'September (9)', 'October (10)', 'November (11)', 'December (12)'];
     setDefaults();
 
-    $scope.$watch('cart', function () {
-      console.log('cart update', $scope.cart);
-
-    });
-
     $scope.updateCart = function (id, quantity) {
       cartService.update(id, quantity || 0).then(function (cart) {
         if (cart.error) {
