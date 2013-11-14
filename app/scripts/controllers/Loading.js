80f31b32ffa2ff4bab06a22202c438aa5c2dc574
Chris Esplin <christopher.esplin@gmail.com>
2013-11-13 17:45:03 -0700

    Making the loading spinner hide for phantomjs calls.

--- app/scripts/controllers/Loading.js
+++ app/scripts/controllers/Loading.js
@@ -1,8 +1,10 @@
 'use strict';
 
 angular.module('islcAngularApp')
-  .controller('LoadingCtrl', function ($scope, $rootScope, $timeout) {
-    var timer;
+  .controller('LoadingCtrl', function ($scope, $rootScope, $timeout, $location) {
+    var timer,
+      FRAGMENT_REGEX = /escaped_fragment/,
+      matches = location.search.match(FRAGMENT_REGEX);
 
     $rootScope.showLoader = function () {
       $scope.hide = false;
@@ -22,8 +24,12 @@ angular.module('islcAngularApp')
       $timeout($rootScope.hideLoader, 500);
     };
 
-    $rootScope.$on('$stateChangeStart', $rootScope.startLoader);
-    $rootScope.$on('$stateChangeSuccess', $rootScope.startLoader);
-    $rootScope.$on('$viewContentLoaded', $rootScope.cancelLoader);
+    if (!matches && matches[0] !== 'escaped_fragment') {
+      $rootScope.$on('$stateChangeStart', $rootScope.startLoader);
+      $rootScope.$on('$stateChangeSuccess', $rootScope.startLoader);
+      $rootScope.$on('$viewContentLoaded', $rootScope.cancelLoader);
+    } else {
+      $scope.hide = true;
+    }
 
   });
