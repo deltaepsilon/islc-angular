0fe1c48c17ca35ccad07a4c80b7dd3d6ec90f2b2
Chris Esplin <christopher.esplin@gmail.com>
2013-11-19 16:59:46 -0700

    Reworking the placeholder polyfill to account for passwords.

--- app/scripts/directives/placeholder.js
+++ app/scripts/directives/placeholder.js
@@ -7,27 +7,37 @@ angular.module('islcAngularApp')
       require: 'ngModel',
       transclude: true,
       link: function postLink(scope, element, attrs) {
+        var type = element.attr('type');
         if (Modernizr.input.placeholder) {
+//          return false;
+        }
+        if (type === 'number' || type === 'integer') {
           return false;
         }
+
         $timeout(function () {
           var placeholder = attrs.placeholder,
-            label = angular.element('<label>' + placeholder + '</label>'),
-            wrapper = angular.element('<div class="input-wrapper"></div>');
+            label;
 
+          if (type === 'password') {
+            label = angular.element('<label class="password-placeholder">' + placeholder + '</label>')
+            label.insertBefore(element);
+          } else {
+            element.val(placeholder);
 
-          element.wrap(wrapper);
-          label.insertBefore(element);
+            element.on('focus', function () {
+              if (element.val() === placeholder) {
+                element.val('');
+              }
+            });
 
-          element.on('focus', function () {
-            label.hide();
-          });
+            element.on('blur', function () {
+              if (!element.val().length) {
+                element.val(placeholder);
+              }
+            });
+          }
 
-          element.on('blur', function () {
-            if (!element.val().length) {
-              label.show();
-            }
-          });
         });
 
 
