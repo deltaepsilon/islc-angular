2bb390802d7d508670fefba611af839f9e86bf30
Chris Esplin <christopher.esplin@gmail.com>
2013-11-19 13:58:04 -0700

    Supporting the updated notifications framework.

--- app/scripts/services/notificationService.js
+++ app/scripts/services/notificationService.js
@@ -3,19 +3,27 @@
 angular.module('islcAngularApp')
   .service('notificationService', function notificationService($notification) {
     $notification.enableHtml5Mode();
+    $notification.setSetting('custom', {duration: 4000, enabled: true});
 
-    return {
+    var service = {
       info: function (title, content, userData) {
-        $notification.notify(null, title, content, userData);
+        $notification.notify(null, title, content, userData, 'notify');
       },
       error: function (title, content, userData) {
-        $notification.notify(null, title, content, userData);
+        $notification.notify(null, title, content, userData, 'error');
       },
       success: function (title, content, userData) {
-        $notification.notify(null, title, content, userData);
+        $notification.notify(null, title, content, userData, 'success');
       },
       warning: function (title, content, userData) {
-        $notification.notify(null, title, content, userData);
+        $notification.notify(null, title, content, userData, 'warning');
       }
-    }
+    };
+
+//    service.info('Notification', 'This is an info message');
+//    service.warning('Notification', 'This is an warning message');
+//    service.error('Notification', 'This is an error message');
+//    service.success('Notification', 'This is an success message');
+
+    return service;
   });
