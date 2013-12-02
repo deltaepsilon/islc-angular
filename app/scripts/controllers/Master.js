a450b36f305546c6d1d15db71a52ad49b6070d8a
Chris Esplin <christopher.esplin@gmail.com>
2013-12-02 11:30:38 -0700

    updating main page title

--- app/scripts/controllers/Master.js
+++ app/scripts/controllers/Master.js
@@ -4,7 +4,7 @@ angular.module('islcAngularApp')
   .controller('MasterCtrl', function ($scope, $rootScope, $filter){
 
     var SLASH_REGEX = /(^#!\/|\/$|\/:\w+|content\/)/g,
-      defaultTitle = 'I Still Love Calligraphy - Join The Class Today!';
+      defaultTitle = 'I Still Love Calligraphy - Learn The Art of Calligraphy Online!';
 
     $scope.title = defaultTitle;
 
