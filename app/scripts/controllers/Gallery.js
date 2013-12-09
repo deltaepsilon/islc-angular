1a639db2bbe8a4ea975931b9c8c81b2cb5ae49d4
Chris Esplin <christopher.esplin@gmail.com>
2013-12-09 12:23:16 -0700

    Working to add back markdown.

--- app/scripts/controllers/Gallery.js
+++ app/scripts/controllers/Gallery.js
@@ -2,11 +2,14 @@
 
 angular.module('islcAngularApp')
   .controller('GalleryCtrl', function ($rootScope, $scope, gallery, galleryService, notificationService, $sanitize) {
+    var LINEFEED_REGEX = /&#10;/g;
+
     $scope.$watch('gallery', function () {
       if ($scope.gallery && $scope.gallery.comments) {
         var i = $scope.gallery.comments.length;
         while (i--) {
           $scope.gallery.comments[i].comment = $sanitize($scope.gallery.comments[i].comment);
+          $scope.gallery.comments[i].comment = $scope.gallery.comments[i].comment.replace(LINEFEED_REGEX, "\n");
         }
       }
 
