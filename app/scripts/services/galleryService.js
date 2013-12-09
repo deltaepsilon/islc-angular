d4b8131e915c3d2ee563e51dd258361d9dcc4ca5
Chris Esplin <christopher.esplin@gmail.com>
2013-12-09 11:19:18 -0700

    Better gallery cache clearing.

--- app/scripts/services/galleryService.js
+++ app/scripts/services/galleryService.js
@@ -3,8 +3,11 @@
 angular.module('islcAngularApp')
   .service('galleryService', function galleryService($rootScope, Restangular, cacheService, commentService) {
     var cache = cacheService.get(),
-      cacheClear = function () {
-        cache.remove('/angular/gallery')
+      cacheClear = function (id) {
+        if (id) {
+          cache.remove('/angular/gallery/' + id);
+        }
+        cache.remove('/angular/gallery');
       };
 
     return {
@@ -17,13 +20,13 @@ angular.module('islcAngularApp')
       },
 
       remove: function (id) {
-        cacheClear();
+        cacheClear(id);
         commentService.clearCache();
         return Restangular.one('gallery', id).remove();
       },
 
       addComment: function (id, comment) {
-        cacheClear();
+        cacheClear(id);
         commentService.clearCache();
         return Restangular.one('gallery', id).all('comment').post({comment: comment});
       },
