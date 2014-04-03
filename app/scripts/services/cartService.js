7186a307959820af1d88616384b43d58c8b682f2
Chris Esplin <christopher.esplin@gmail.com>
2014-04-02 20:37:11 -0600

    Tracking cart adds by both page and by product id.

--- app/scripts/services/cartService.js
+++ app/scripts/services/cartService.js
@@ -48,7 +48,8 @@ angular.module('islcAngularApp')
           quantity: quantity || 1
         }
 
-        Analytics.trackEvent('cart', $state.current.name, productId, quantity);
+        Analytics.trackEvent('cart by page', $state.current.name, productId, quantity);
+        Analytics.trackEvent('cart by product', productId, $state.current.name, quantity);
 
         return Restangular.all('cart').post(query);
       },
